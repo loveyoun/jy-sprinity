cac7861194104c77a76e17056461ef4c8139dff2
loveyoun <lovekite0406@gmail.com>
2022-11-28 14:59:13 +0900

    initial commit

---
+++ src/main/java/sprinity/jysprinity/JySprinityApplication.java
@@ -0,0 +1,13 @@
+package sprinity.jysprinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JySprinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JySprinityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sprinity/jysprinity/JySprinityApplicationTests.java
@@ -0,0 +1,13 @@
+package sprinity.jysprinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JySprinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
