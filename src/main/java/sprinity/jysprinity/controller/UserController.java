171df2d745d78541c663603c83754c5f88c782da
loveyoun <lovekite0406@gmail.com>
2022-12-02 15:46:00 +0900

    로그인 및 로그아웃 기능과 화면 구현

--- src/main/java/sprinity/jysprinity/controller/UserController.java
+++ src/main/java/sprinity/jysprinity/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import sprinity.jysprinity.domain.User;
 import sprinity.jysprinity.repository.UserRepository;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,16 +19,42 @@ public class UserController {
     private UserRepository userRepository;
     //private List<User> users = new ArrayList<User>(); list collection
 
+    /*
+     * 로 그 인
+     * */
+    @GetMapping("/login")
+    public String loginForm() {
+        return "/user/login";
+    }
+
+    @PostMapping("/login")
+    public String login(String userId, String password, HttpSession session) {
+        User user = userRepository.findByUserId(userId);
+        if (user == null) return "redirect:/users/login";
+        if (!password.equals(user.getPassword())) return "redirect:/users/login";
+        session.setAttribute("user", user);
+        return "redirect:/";
+    }
+
+    /*
+     * 로 그 아 웃
+     * */
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("user");
+        return "redirect:/";
+    }
+
     @GetMapping("/form")    //static.user.form.html이 아니라, template.user.form.html 접근 위해서
-    public String form(){
+    public String form() {
         return "/user/form";
     }
-    
+
     /*서버로 데이터 받은 것*/
     @PostMapping("")    //RequestMapping 때문에. @PostMapping("/user/create")
     //@GetMapping("/create"). 보안 issue
-    public String create_f(User user){
-    //public String create(String userId, String password, String name, String email)
+    public String create_f(User user) {
+        //public String create(String userId, String password, String name, String email)
         //System.out.println("user: " + user); toString()으로 가능
         //users.add(user);
         userRepository.save(user);
@@ -39,25 +66,26 @@ public class UserController {
     @GetMapping("") //Post랑 Get이니까 "/users" 같아도 됨
     //@GetMapping("/user/list") 회원목록 받아오는 거니까 get. localhost:8080/user/list 로 url을 받으면,
     //list 이름 다 같아도 되네???
-    public String list_f(Model model){  //html로 보내기 위해서 model이 필요하다
+    public String list_f(Model model) {  //html로 보내기 위해서 model이 필요하다
         //model.addAttribute("users", users);
         model.addAttribute("users", userRepository.findAll());
         return "/user/list";
     }
 
     /*
-    **회원정보 수정**
-    */
+     **회원정보 수정**
+     */
     @GetMapping("/{id}/form")  //url에서 id값 얻어와야 한다. @PathVariable 과 같은 이름
     //이럴 떄, html의 css의 경로를 절대경로로 바꾸어 주어야 한다
-    public String updateForm(@PathVariable Long id, Model model){
+    public String updateForm(@PathVariable Long id, Model model) {
         //User user = userRepository.findOne(id);
         User user = userRepository.findById(id).get(); //Optional
         model.addAttribute("user", user);
         return "/user/updateForm";
     }
+
     @PostMapping("/{id}")
-    public String update(@PathVariable Long id, User newUser){
+    public String update(@PathVariable Long id, User newUser) {
         User user = userRepository.findById(id).get();
         user.update(newUser);
         userRepository.save(user); //id가 없으면 insert, 있으면 update
--- src/main/java/sprinity/jysprinity/repository/UserRepository.java
+++ src/main/java/sprinity/jysprinity/repository/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import sprinity.jysprinity.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-//Database에 저장 조회 기능구현 끝!
-//이 자체가 API임
+    //Database에 저장 조회 기능구현 끝! 이 자체가 API임
+    User findByUserId(String userId);
 }
